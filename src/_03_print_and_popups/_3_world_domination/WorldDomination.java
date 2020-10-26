f6f61408f8ffed622eb7b9c89d7702c2a1b0c673
root <root@052060f14f8b>
2020-10-26 20:28:18 +0000

    DragonFight

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,9 +8,14 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String answer = JOptionPane.showInputDialog("Do you know how to code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if (answer.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null, "You will rule the world");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Sign up for classes at The League");
+		}
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,38 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+			
 				// 3.  Use an if statement to check if their answer is correct
-				
+			String answer = JOptionPane.showInputDialog("What does 2 + 2 equal?");
+				if(answer.equalsIgnoreCase("4")) {
+					score += 1;
+				}
+				else {
+					score -= 1;
+				}
+			String answer1 = JOptionPane.showInputDialog("What does 5 + 2 equal?");
+				if(answer1.equalsIgnoreCase("7")) {
+					score += 1;
+				}
+				else {
+					score -= 1;
+				}
 				// 4.  if the user's answer was correct, add one to their score 
-		
+		//System.out.println(score);
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
 		
 		
 		// After all the questions have been asked, tell the user their final score 
-		
+		JOptionPane.showMessageDialog(null, "Your score is " + score);
 	}
 }
